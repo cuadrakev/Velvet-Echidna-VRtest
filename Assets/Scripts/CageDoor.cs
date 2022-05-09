672c76f215a64a41d39d2d50b751beb90f4a6e35
WDobies <68401136+WDobies@users.noreply.github.com>
2022-05-09 19:33:42 +0200

    Set start position

--- Assets/Scripts/CageDoor.cs
+++ Assets/Scripts/CageDoor.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CageDoor : MonoBehaviour
 {
+    public GameObject floor;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("KeyRusty"))
         {
             transform.Rotate(0, 0, -90);
+            floor.SetActive(true);
         }
     }
 }
